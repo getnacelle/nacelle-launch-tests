7af5bbf3f3baa3853dcb654f55d2ace8f6c2f59e
Nick Richmond <nwrichmond@turnt.tech>
2020-01-21 15:49:03 -0500

    Add and configure plugins

--- gatsby-shopify/gatsby-config.js
+++ gatsby-shopify/gatsby-config.js
@@ -1,9 +1,45 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 module.exports = {
-  /* Your site config here */
-}
+  siteMetadata: {
+    title: `Nacelle Launch Test: Gatsby Shopify`,
+    description: `This project uses Shopify for a 
+    PIM & CMS, with a Gatsby frontend.`,
+    author: `Nacelle Inc. (getnacelle.com)`
+  },
+  plugins: [
+    {
+      resolve: `@nacelle/gatsby-theme-nacelle`,
+      options: {
+        nacelle_space_id: process.env.NACELLE_SPACE_ID,
+        nacelle_graphql_token: process.env.NACELLE_GRAPHQL_TOKEN
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-nacelle-launch-test`,
+        short_name: `nacelle-gatsby-shopify`,
+        start_url: `/`,
+        background_color: `#2846dc`,
+        theme_color: `#2846dc`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon/favicon-32x32.png`
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`, 
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`
+  ]
+};
