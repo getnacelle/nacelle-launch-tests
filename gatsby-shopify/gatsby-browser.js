0cfcd021c4a1efe5289dd124e5214e5769cfbd5a
Nick Richmond <nwrichmond@turnt.tech>
2020-01-23 12:07:44 -0500

    Move cart out of top of cmpnt tree

--- gatsby-shopify/gatsby-browser.js
+++ gatsby-shopify/gatsby-browser.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/state/store-with-middleware';
-import Cart from './src/components/Cart';
 
 export const wrapRootElement = ({ element }) => (
-  <Provider store={store}>
-    {element}
-    <Cart />
-  </Provider>
+  <Provider store={store}>{element}</Provider>
 );
