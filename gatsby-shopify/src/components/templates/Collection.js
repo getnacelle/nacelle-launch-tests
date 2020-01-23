138220cc21ec2534437f3b58b09b32969c176d52
Nick Richmond <nwrichmond@turnt.tech>
2020-01-23 11:36:34 -0500

    Move product filtering out of cmpnt; Add layout
    The product filtering now lives in gatsby-node.js

--- gatsby-shopify/src/components/templates/Collection.js
+++ gatsby-shopify/src/components/templates/Collection.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Product } from '../Product';
+import Layout from './Layout';
 
 const Collection = ({ pageContext }) => {
-  const { title, imageSrc, handles, allProducts } = pageContext;
-  const products = allProducts.filter(product =>
-    handles.includes(product.handle)
-  );
+  const { title, imageSrc, products } = pageContext;
   return (
-    <div style={{ position: 'relative' }}>
-      <h1>{title}</h1>
-      <img src={imageSrc} alt={title} style={{ width: '30em' }} />
-      <ul style={{ listStyleType: 'none' }}>
-        {products.map(el => (
-          <li key={el.handle}>
-            <Product
-              title={el.title}
-              handle={el.handle}
-              src={el.featuredMedia.src}
-              variants={el.variants}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Layout>
+      <div style={{ position: 'relative' }}>
+        <h1>{title}</h1>
+        <img src={imageSrc} alt={title} style={{ width: '30em' }} />
+        <ul style={{ listStyleType: 'none' }}>
+          {products.map(el => (
+            <li key={el.handle}>
+              <Product
+                title={el.title}
+                handle={el.handle}
+                src={el.featuredMedia.src}
+                variants={el.variants}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
   );
 };
 
