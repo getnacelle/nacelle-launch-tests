a2ee4dfbba05d89292803131dee207531ce73cac
Nick Richmond <nwrichmond@turnt.tech>
2020-01-24 15:04:27 -0500

    Add hero image component

---
+++ gatsby-shopify/src/components/HeroImage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import Image from './Image';
+
+const HeroContainer = styled.div`
+  margin: 0 auto;
+  position: relative;
+  width: 100%;
+  min-height: 18em;
+  display: flex;
+  align-items: center;
+  picture {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  div {
+    margin: 0 auto;
+    padding: 1em;
+    border: 1px solid white;
+    border-radius: 4px;
+    background-color: rgba(255, 255, 255, 95%);
+    text-align: center;
+    h1 {
+      margin: 0;
+    }
+  }
+`;
+
+const HeroImage = ({ title, src }) => (
+  <HeroContainer>
+    <Image src={src} alt={title} />
+    {title && (
+      <div>
+        <h1>{title}</h1>
+      </div>
+    )}
+  </HeroContainer>
+);
+
+export default HeroImage;
