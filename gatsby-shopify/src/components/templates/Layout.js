f967e8d0a3bd4845f7d40e3801446465784597fd
Nick Richmond <nwrichmond@turnt.tech>
2020-01-24 14:58:47 -0500

    Center content in main

--- gatsby-shopify/src/components/templates/Layout.js
+++ gatsby-shopify/src/components/templates/Layout.js
@@ -8,10 +8,19 @@ const Container = styled.div`
   min-height: 100vh;
 `;
 
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  h1 {
+    margin-top: 1em;
+  }
+`;
+
 const Layout = ({ children }) => (
   <Container>
     <Header />
-    <main>{children}</main>
+    <Main>{children}</Main>
   </Container>
 );
 
