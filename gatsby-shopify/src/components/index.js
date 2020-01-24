764d20aac3dacd6efebea3a0e308b95ad0669b0a
Nick Richmond <nwrichmond@turnt.tech>
2020-01-24 10:57:10 -0500

    Set up named imports from components dir
    Example:
    import { Product, Cart } from 'src/components'

---
+++ gatsby-shopify/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Cart } from './Cart';
+export { default as Header } from './Header';
+export { default as HeroImage } from './HeroImage';
+export { default as Logo } from './Logo';
+export { default as Product } from './Product';
+export { default as Products } from './Products';
