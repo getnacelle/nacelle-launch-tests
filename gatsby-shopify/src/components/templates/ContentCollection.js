8debcb4739fdb1dc0e0b4f71c040178f91318cf5
Nick Richmond <nwrichmond@turnt.tech>
2020-01-24 18:26:38 -0500

    Split templates into components

--- gatsby-shopify/src/components/templates/ContentCollection.js
+++ gatsby-shopify/src/components/templates/ContentCollection.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'gatsby';
-import { HeroImage, Products } from 'src/components';
-import Layout from './Layout';
+import { HeroImage, Products, Landing, Blog } from 'src/components';
+import Layout from 'src/components/Layout';
 
-const FlexCenter = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const BlogArticle = styled.article`
-  width: 70%;
-  max-width: 800px;
-  margin: 1em;
-  padding: 1em;
-  border: 1px solid rgba(0, 0, 0, 20%);
-  border-radius: 3px;
-  a {
-    text-decoration: none;
-  }
-`;
-
-const CollectionItem = ({ item, page }) => {
-  const { title, handle } = item;
+const CollectionItem = ({ item, page, recentArrivals }) => {
+  const { title } = item;
   const src = item.featuredMedia ? item.featuredMedia.src : null;
   if (page === 'shop') {
     return (
@@ -32,32 +13,27 @@ const CollectionItem = ({ item, page }) => {
     );
   }
   if (page === 'blog') {
-    return (
-      <FlexCenter>
-        <BlogArticle>
-          <h2>{handle && <Link to={`/${page}/${handle}`}>{title}</Link>}</h2>
-          {src && <HeroImage src={src} alt={title} />}
-        </BlogArticle>
-      </FlexCenter>
-    );
+    return <Blog item={item} page={page} />;
   }
   if (page === 'homepage') {
-    return (
-      <article>
-        {src && <HeroImage src={src} alt={title} title={title} />}
-      </article>
-    );
+    return <Landing item={item} recentArrivals={recentArrivals} />;
   }
-  return (
-    <BlogArticle>
-      <h2>{handle && <Link to={`/${page}/${handle}`}>{title}</Link>}</h2>
-      {src && <HeroImage src={src} alt={title} />}
-    </BlogArticle>
-  );
 };
 
 const ContentPage = ({ pageContext }) => {
   const { handle, content, collection, products } = pageContext;
+  let recentArrivals;
+  if (handle === 'homepage' && products !== null) {
+    const getLatestArrivalTimes = limit =>
+      products
+        .map(el => el.createdAt)
+        .sort()
+        .slice(0, limit);
+    const fourMostRecentTimes = getLatestArrivalTimes(4);
+    recentArrivals = products.filter(el =>
+      fourMostRecentTimes.includes(el.createdAt)
+    );
+  }
   return (
     <Layout>
       {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
@@ -68,10 +44,11 @@ const ContentPage = ({ pageContext }) => {
               item={el}
               page={handle}
               key={`${el.title}-${idx}`}
+              recentArrivals={recentArrivals}
             />
           )
       )}
-      {products && <Products products={products} />}
+      {products && handle !== 'homepage' && <Products products={products} />}
     </Layout>
   );
 };
