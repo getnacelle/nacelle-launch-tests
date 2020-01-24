b765ad16e6b659069858defea242ea365b834142
Nick Richmond <nwrichmond@turnt.tech>
2020-01-24 15:05:09 -0500

    Prefer components to complex templating

--- gatsby-shopify/src/components/templates/Collection.js
+++ gatsby-shopify/src/components/templates/Collection.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Product } from '../Product';
+import { HeroImage, Products } from 'src/components';
 import Layout from './Layout';
 
 const Collection = ({ pageContext }) => {
   const { title, imageSrc, products } = pageContext;
   return (
     <Layout>
-      <div style={{ position: 'relative' }}>
-        <h1>{title}</h1>
-        <img src={imageSrc} alt={title} style={{ width: '30em' }} />
-        <ul style={{ listStyleType: 'none' }}>
-          {products.map(el => (
-            <li key={el.handle}>
-              <Product
-                title={el.title}
-                handle={el.handle}
-                src={el.featuredMedia.src}
-                variants={el.variants}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <HeroImage src={imageSrc} title={title} />
+      <Products products={products} />
     </Layout>
   );
 };
