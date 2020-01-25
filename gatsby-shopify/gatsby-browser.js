e160bf9f3cf20c02289eca77b71b89c879dfd570
Nick Richmond <nwrichmond@turnt.tech>
2020-01-24 21:25:43 -0500

    Use absolute imports

--- gatsby-shopify/gatsby-browser.js
+++ gatsby-shopify/gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './src/state/store-with-middleware';
+import { store } from 'src/state/store-with-middleware';
 
 export const wrapRootElement = ({ element }) => (
   <Provider store={store}>{element}</Provider>
--- gatsby-shopify/gatsby-ssr.js
+++ gatsby-shopify/gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './src/state/store-without-middleware';
+import { store } from 'src/state/store-without-middleware';
 
 export const wrapRootElement = ({ element }) => (
   <Provider store={store}>{element}</Provider>
