cd1b9d4757579e756e506bfe59c2ddc1ca6da5bc
Nick Richmond <nwrichmond@turnt.tech>
2020-01-27 22:21:31 -0500

    Stack content in mobile view

--- gatsby-shopify/src/components/templates/ProductDetail.js
+++ gatsby-shopify/src/components/templates/ProductDetail.js
@@ -1,44 +1,54 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Layout from 'src/components/Layout';
 import { Product, SEO } from 'src/components';
 
-const CenteredTextBlock = styled.div`
-  text-align: center;
-  padding-top: 2em;
+const Container = styled.div`
+  margin: 0 auto;
+  width: ${({ isMobile }) => (isMobile ? '80%' : 'auto')};
+  max-width: 50em;
 `;
 
 const SideBySidePanel = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: ${({ isMobile }) => (isMobile ? '1fr' : '1fr 1fr')};
   text-align: center;
-  border: 1px solid rgba(0, 0, 0, 10%);
   div {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 2em;
+    margin-top: 2em;
+    margin-bottom: 2em;
+    article {
+      padding-left: 0;
+      padding-right: 0;
+    }
     p {
       max-width: 30em;
+      padding: 0 2em;
     }
   }
 `;
 
 const ProductDetail = ({ pageContext }) => {
   const { title, handle, description, imageSrc, variants } = pageContext;
+  const isMobile = useSelector(state => state.user.isMobile);
   return (
     <Layout>
       <SEO title={title} />
-      <SideBySidePanel>
-        <div>
-          <Product handle={handle} src={imageSrc} variants={variants} />
-        </div>
-        <div>
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
-      </SideBySidePanel>
+      <Container isMobile={isMobile}>
+        <SideBySidePanel isMobile={isMobile}>
+          <div>
+            <Product handle={handle} src={imageSrc} variants={variants} />
+          </div>
+          <div>
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </div>
+        </SideBySidePanel>
+      </Container>
     </Layout>
   );
 };
