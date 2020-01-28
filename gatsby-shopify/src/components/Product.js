1d8d0f6ab16f8a0e66917b54ff385d1dde2b13eb
Nick Richmond <nwrichmond@turnt.tech>
2020-01-27 22:17:04 -0500

    Make "Add to Cart" button match cart button style
    Also formatting

--- gatsby-shopify/src/components/Product.js
+++ gatsby-shopify/src/components/Product.js
@@ -1,6 +1,5 @@
 import React, { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'gatsby';
 import styled from 'styled-components';
 import Image from './Image';
 import { addToCart } from '../state/cart-actions';
@@ -8,13 +7,14 @@ import { addToCart } from '../state/cart-actions';
 const ProductCard = styled.article`
   display: grid;
   grid-template-rows: 4em 1fr 4em 3em 2em;
+  grid-column-gap: 2em;
   justify-content: center;
   text-align: center;
   min-width: 20%;
-  margin: 0 2em;
-  padding: 3em;
+  padding: 2em;
   border: 1px solid rgba(0, 0, 0, 15%);
   border-radius: 0.3em;
+  color: #212736;
   a {
     text-decoration: none;
     font-size: 0.8em;
@@ -22,6 +22,24 @@ const ProductCard = styled.article`
   select {
     margin: 1em;
   }
+`;
+
+const ButtonContainer = styled.div`
+  margin: 1em 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 3em;
+`;
+
+const CartButton = styled.button`
+  border: 1px solid rgba(0, 0, 0, 10%);
+  width: 12em;
+  border-radius: 5px;
+  padding: 0.25em 2em;
+  background-color: #212736;
+  color: white;
+  cursor: pointer;
 `;
 
 const Product = ({ title, handle, src, variants }) => {
@@ -43,9 +61,7 @@ const Product = ({ title, handle, src, variants }) => {
   );
   return (
     <ProductCard>
-      <h2>
-        {handle ? <Link to={`/products/${handle}`}>{title}</Link> : title}
-      </h2>
+      <h2>{title}</h2>
       {src && <Image src={src} alt={title} />}
       {hasMultipleVariants ? (
         <div>
@@ -61,9 +77,11 @@ const Product = ({ title, handle, src, variants }) => {
         <div />
       )}
       <p>$ {Number(selectedVariant.price).toFixed(2)}</p>
-      <button type="button" onClick={addToCartMemo}>
-        Add To Cart
-      </button>
+      <ButtonContainer>
+        <CartButton type="button" onClick={addToCartMemo}>
+          Add To Cart
+        </CartButton>
+      </ButtonContainer>
     </ProductCard>
   );
 };
