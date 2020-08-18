e9ecf338bd688b2113df30e1f791bb44aaea448c
Caleb Bodtorf <cbodtorf@gmail.com>
2020-08-18 15:33:16 -0700

    permalinks initial commit.

---
+++ nuxt-shopify-permalinks/pages/checkout/complete.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+<script>
+export default {
+  head: {
+    script: [
+      { src: '/checkout-referrer-head-check.js' },
+    ]
+  },
+  mounted() {
+    // TODO Guard Route if token is not available
+    token = this.$route.query.token
+    console.log('nacelle token', token)
+  },
+}
+</script>
